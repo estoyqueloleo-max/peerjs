6f0701c5cbe32375e886ec22bd79e2ea4d36f155
Jose Juan Montiel <josejuan.montiel@gmail.com>
2026-04-30 00:03:18 +0200

    Fix deploy

--- main.go
+++ main.go
@@ -3,28 +3,35 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 
 	peerjs "github.com/muka/peerjs-go/server"
 )
 
 func main() {
 	// 1. Render inyecta el puerto en la variable de entorno PORT
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "9000" // Puerto por defecto para local
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		portStr = "9000" // Puerto por defecto para local
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatalf("Puerto inválido: %v", err)
 	}
 
 	// 2. Configuración del servidor PeerJS
 	opts := peerjs.NewOptions()
 	opts.Port = port
+	opts.Host = "0.0.0.0"
 	opts.Path = "/myapp" // Puedes cambiar esto por el nombre de tu app
 	opts.AllowDiscovery = true
 
-	server := peerjs.NewServer(opts)
+	server := peerjs.New(opts)
 	defer server.Stop()
 
 	// 3. Canal para capturar errores de inicio
-	log.Printf("Iniciando servidor PeerJS en Go sobre el puerto %s...", port)
+	log.Printf("Iniciando servidor PeerJS en Go sobre %s:%d%s...", opts.Host, port, opts.Path)
 
 	// El servidor de muka maneja internamente el servidor HTTP
 	if err := server.Start(); err != nil {
